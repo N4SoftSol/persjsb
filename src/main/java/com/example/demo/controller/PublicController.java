159c4e40e63097f4d9b6015601f32821b0c2d205
Naveen Paulraj <Naveen.Paulraj@n4softsol.com>
2026-09-15 10:37:25 -0400

    Full CI/CD platform complete

--- src/main/java/com/example/demo/controller/PublicController.java
+++ src/main/java/com/example/demo/controller/PublicController.java
@@ -29,7 +29,7 @@ public class PublicController {
                 new HashMap<>();
         info.put("status", "UP");
         info.put("application", "api-server");
-        info.put("Git Update", "2.0");
+        info.put("Git Update", "6.0");
         info.put(
                 "profiles",
                 environment.getActiveProfiles());
