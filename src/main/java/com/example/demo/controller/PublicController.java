b87520e8edc1013edc4abfd024ac610c1ca608cf
Naveen Paulraj <Naveen.Paulraj@n4softsol.com>
2026-09-15 11:37:15 -0400

    Refactor Public Api controller for app name & app version

--- src/main/java/com/example/demo/controller/PublicController.java
+++ src/main/java/com/example/demo/controller/PublicController.java
@@ -22,7 +22,7 @@ public class PublicController {
 
     public PublicController(
             Environment environment,
-            @Value("${info.app.name}") String appName,
+            @Value("${info.application.name}") String appName,
             @Value("${info.app.version}") String appVersion
 
     ) {
